4ae00b398d68d7faf93a1673ee7aacaccc38eaa1
penguin-dude123 <73714848+penguin-dude123@users.noreply.github.com>
2022-03-01 13:31:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ function update(progress){
         }
     }
     //restart
-    if(playerState.pressedKeys.r || playerState.pressedKeys.up){
+    if(playerState.pressedKeys.r){
         reset();
     }
     //score
